b1e20d6550278dccdd67dbb08a43861fa0214082
Karolina Drazewska <drazewska.karolina@gmail.com>
2020-09-03 18:13:39 +0200

    Basic application state

---
+++ spacer/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app">
+    <nav>
+      <router-link to="/">Spacer</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
